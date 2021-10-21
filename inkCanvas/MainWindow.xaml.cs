1888e0ed079899af0607f777ace071e779f0a716
Anthony Biccum <atbiccum@gmail.com>
2021-10-21 15:20:17 -0700

    Added save and load options. Plus some XAML for basic buttons.

--- inkCanvas/MainWindow.xaml.cs
+++ inkCanvas/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Ink;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -26,6 +29,35 @@ namespace inkCanvas
         }
 
         private void buttSave_Click(object sender, RoutedEventArgs e)
+        {
+            //Code from: https://docs.microsoft.com/en-us/dotnet/desktop/wpf/advanced/storing-ink?view=netframeworkdesktop-4.8
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "isf files (*.isf)|*.isf";
+
+            if (saveFileDialog1.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(saveFileDialog1.FileName,
+                                               FileMode.Create);
+                theInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+        }
+
+        private void buttOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "isf files (*.isf)|*.isf";
+
+            if (openFileDialog1.ShowDialog() == true)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName,
+                                               FileMode.Open);
+                theInkCanvas.Strokes = new StrokeCollection(fs);
+                fs.Close();
+            }
+        }
+
+        private void buttClear_Click(object sender, RoutedEventArgs e)
         {
 
         }
